ab574a0ece36a5c7f292ff0dd125fed76cac2787
clliu <clliu@bigpond.com>
2018-03-03 16:00:13 +1100

    Add in sample code and issue doc

--- Analysis.py
+++ Analysis.py
@@ -1,4 +1,14 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
+df_swing = pd.read_csv('2008_swing_states.csv')
 
+df_swing[['state', 'county', 'dem_share']]
+
+sns.set()
+a = plt.hist(df_swing['dem_share'])
+a = plt.xlabel('percent of vote for Obama')
+a = plt.ylabel('number of counties')
+
+plt.show()
